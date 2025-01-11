21f377bf463a06d9310c2af61ebab82f05ffc6ff
Ahmdpolash <ahmedpolash732@gmail.com>
2025-01-11 11:49:10 +0600

    learn all ddl commands

--- alter.sql
+++ alter.sql
@@ -1,63 +1,54 @@
 SELECT * from person2;
 
+--- creating a new table
+
+CREATE TABLE test_table (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(25) NOT NULL
+)
+
 ALTER TABLE person2
-    ADD COLUMN email VARCHAR(20) DEFAULT 'default@mail.com' NOT NULL,
-    ADD COLUMN isActive BOOLEAN DEFAULT 'true'
+ADD COLUMN email VARCHAR(20) DEFAULT 'default@mail.com' NOT NULL,
+ADD COLUMN isActive BOOLEAN DEFAULT 'true'
 
 -- delete a column from table
 ALTER Table person2 DROP COLUMN email
 
-
 -- insert a new column
-INSERT INTO person2 values ( 4, 'john', 45, 'test@gmail.com' )
+INSERT INTO
+    person2
+values (
+        4,
+        'john',
+        45,
+        'test@gmail.com'
+    )
+    -- rename table column name
+ALTER Table person2
+RENAME COLUMN persorn_age to person_age
 
--- rename table column name
-ALTER Table person2 
-   RENAME COLUMN persorn_age to person_age
-
-
-
---update a existign data type 
-ALTER Table person2 
-   alter COLUMN user_name type VARCHAR(30)
+--update a existign data type
+ALTER Table person2 alter COLUMN user_name type VARCHAR(30)
 
 --add a constraint value to column
-ALTER Table person2 
-   alter COLUMN person_age set NOT NULL 
+ALTER Table person2 alter COLUMN person_age set NOT NULL
 
 -- remove a constraint value from column
-ALTER Table person2 
-   alter COLUMN person_age DROP NOT NULL
+ALTER Table person2 alter COLUMN person_age DROP NOT NULL
 
+--------------- TODO:  day two ------------------------
 
+-- set a unique constraint value
 
+ALTER Table person2
+ADD constraint unique_person2_person_age UNIQUE (person_age)
 
+-- remove  unique constraint value from column
 
+ALTER Table person2 DROP constraint unique_person2_person_age
 
+--- to delete a entrie table
+DROP TABLE person2
 
-
-
--------------- practice table --------------------------------
-   -- give me some task with relevent upper code 
-   -- add a new column to table
-    -- delete a column from table
-    -- rename table column name
-    -- update a existign data type
-    -- add a constraint value to column
-    -- remove a constraint value from column
-    -- add a new column to table
-    -- delete a column from table
-
-
----------------------------- practice ------------------------
-
-ALTER TABLE person3 
-    ADD COLUMN email_address VARCHAR(25) DEFAULT 'default@example.com'
-
-ALTER Table person3 DROP COLUMN age
-
-ALTER Table person3 RENAME id to primary_id
-
-ALTER Table person3 alter COLUMN user_name type CHARACTER
-
-ALTER Table person3 alter COLUMN email_address set not null 
+-- to delete entire table data
+TRUNCATE TABLE person2
---
+++ practice.sql
@@ -0,0 +1,31 @@
+-------------- practice table --------------------------------
+   -- give me some task with relevent upper code 
+   -- add a new column to table
+    -- delete a column from table
+    -- rename table column name
+    -- update a existign data type
+    -- add a constraint value to column
+    -- remove a constraint value from column
+    -- add a new column to table
+    -- delete a column from table
+
+
+---------------------------- practice ------------------------
+
+ALTER TABLE person3 
+    ADD COLUMN email_address VARCHAR(25) DEFAULT 'default@example.com'
+
+ALTER Table person3 DROP COLUMN age
+
+ALTER Table person3 RENAME id to primary_id
+
+ALTER Table person3 alter COLUMN user_name type CHARACTER
+
+ALTER Table person3 alter COLUMN email_address set not null 
+
+
+----- day two -----
+
+
+alter table test_table
+add constraint UNIQUE_test_table_name UNIQUE (name)
