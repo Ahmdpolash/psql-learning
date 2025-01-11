24359ad56d9204a2e6ffaf5ada83f05c6924ac0b
Ahmdpolash <ahmedpolash732@gmail.com>
2025-01-11 12:40:48 +0600

    solve some task using  sorting method (where, and using logical operator)

--- practice.sql
+++ practice.sql
@@ -1,19 +1,18 @@
 -------------- practice table --------------------------------
-   -- give me some task with relevent upper code 
-   -- add a new column to table
-    -- delete a column from table
-    -- rename table column name
-    -- update a existign data type
-    -- add a constraint value to column
-    -- remove a constraint value from column
-    -- add a new column to table
-    -- delete a column from table
-
+-- give me some task with relevent upper code
+-- add a new column to table
+-- delete a column from table
+-- rename table column name
+-- update a existign data type
+-- add a constraint value to column
+-- remove a constraint value from column
+-- add a new column to table
+-- delete a column from table
 
 ---------------------------- practice ------------------------
 
-ALTER TABLE person3 
-    ADD COLUMN email_address VARCHAR(25) DEFAULT 'default@example.com'
+ALTER TABLE person3
+ADD COLUMN email_address VARCHAR(25) DEFAULT 'default@example.com'
 
 ALTER Table person3 DROP COLUMN age
 
@@ -21,11 +20,14 @@ ALTER Table person3 RENAME id to primary_id
 
 ALTER Table person3 alter COLUMN user_name type CHARACTER
 
-ALTER Table person3 alter COLUMN email_address set not null 
-
+ALTER Table person3 alter COLUMN email_address set not null
 
 ----- day two -----
-
+-- add a unique/pk constraint value
+-- remove a constraint value from column
+-- delete a table
+-- delete entire table data
+-- order by ,DISTINCT, where ,group by
 
 alter table test_table
 add constraint UNIQUE_test_table_name UNIQUE (name)
---
+++ select.sql
@@ -0,0 +1,169 @@
+CREATE TABLE students (
+    student_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(25) NOT NULL,
+    last_name VARCHAR(25) NOT NULL,
+    age INT CHECK (age >= 20),
+    grade CHAR(2),
+    course VARCHAR(50),
+    email VARCHAR(100) UNIQUE,
+    dob DATE,
+    blood_group VARCHAR(5),
+    country VARCHAR(50)
+)
+
+--- insert students table data
+
+INSERT INTO
+    students (
+        first_name,
+        last_name,
+        age,
+        grade,
+        course,
+        email,
+        dob,
+        blood_group,
+        country
+    )
+VALUES (
+        'John',
+        'Doe',
+        20,
+        'A',
+        'Computer Science',
+        'john.doe@example.com',
+        '2003-05-14',
+        'O+',
+        'USA'
+    ),
+    (
+        'Jane',
+        'Smith',
+        22,
+        'B',
+        'Mathematics',
+        'jane.smith@example.com',
+        '2001-03-22',
+        'A-',
+        'Canada'
+    ),
+    (
+        'Michael',
+        'Johnson',
+        19,
+        'A',
+        'Physics',
+        'michael.johnson@example.com',
+        '2004-08-12',
+        'B+',
+        'UK'
+    ),
+    (
+        'Emily',
+        'Brown',
+        21,
+        'C',
+        'Biology',
+        'emily.brown@example.com',
+        '2002-01-18',
+        'AB-',
+        'Australia'
+    ),
+    (
+        'David',
+        'Wilson',
+        23,
+        'B',
+        'Engineering',
+        'david.wilson@example.com',
+        '2000-09-25',
+        'O-',
+        'India'
+    ),
+    (
+        'Sophia',
+        'Taylor',
+        18,
+        'A',
+        'Chemistry',
+        'sophia.taylor@example.com',
+        '2005-07-30',
+        'A+',
+        'Germany'
+    ),
+    (
+        'James',
+        'Anderson',
+        20,
+        'B',
+        'History',
+        'james.anderson@example.com',
+        '2003-04-10',
+        'B-',
+        'France'
+    ),
+    (
+        'Olivia',
+        'Thomas',
+        22,
+        'A',
+        'Literature',
+        'olivia.thomas@example.com',
+        '2001-12-05',
+        'O+',
+        'Italy'
+    ),
+    (
+        'Liam',
+        'Moore',
+        19,
+        'C',
+        'Economics',
+        'liam.moore@example.com',
+        '2004-06-15',
+        'AB+',
+        'Japan'
+    ),
+    (
+        'Isabella',
+        'Jackson',
+        21,
+        'B',
+        'Philosophy',
+        'isabella.jackson@example.com',
+        '2002-11-08',
+        'A-',
+        'South Africa'
+    );
+
+-- retrive data from table
+SELECT * FROM students
+
+-- rename a column name using as
+SELECT email as student_email FROM students
+
+-- sorting ascending using order by email
+SELECT * FROM students ORDER BY age ASC
+
+-- get unique data count
+SELECT DISTINCT blood_group from students
+
+-- data filtering
+-- select student from USA
+--select student with A grade in physics
+--select student with specific blood group (A)
+--select student from usa or australia
+--select student from usa or australia and age is 25
+--select student whose age is older than 21
+
+SELECT * from students WHERE country = 'USA'
+
+SELECT * FROM students WHERE grade = 'A' AND course = 'Physics'
+
+SELECT * from students WHERE blood_group = 'A+'
+
+SELECT * from students WHERE (country = 'USA' OR country = 'Australia')
+
+SELECT * FROM students WHERE (country = 'USA' OR country = 'Australia') and age = 20
+
+SELECT * FROM students WHERE age > 21 and course = 'Mathematics'
