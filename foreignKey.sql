c053b5b84c369ad28bdd91720711f110fee6841f
Ahmdpolash <ahmedpolash732@gmail.com>
2025-01-13 12:23:20 +0600

    learn foreign key insert and deletetion restriction handling

--- foreignKey.sql
+++ foreignKey.sql
@@ -11,8 +11,14 @@ CREATE TABLE "user" (
 CREATE TABLE post (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
-    userId INTEGER REFERENCES "user" (id)
+    -- userId INTEGER REFERENCES "user" (id) ON DELETE CASCADE
+    -- userId INTEGER REFERENCES "user" (id) ON DELETE set null
+    userId INTEGER REFERENCES "user" (id) on delete set DEFAULT DEFAULT 2 
 )
+
+DROP Table post
+
+DROP Table "user"
 
 ALTER table post ALTER COLUMN userId DROP NOT NULL
 
@@ -53,4 +59,17 @@ SELECT * FROM "user"
 
 SELECT * FROM post
 
-INSERT INTO post (title, userId) VALUES ('test', NULL)
+INSERT INTO post (title, userId) VALUES ('test', 0)
+
+-- todo Insertion constraint on INSERT post
+-- Attempting to insert a post with a user ID that does not exist
+-- Inserting a post with a valid user ID
+-- Attempting to insert a post without specifying a user ID
+
+DELETE FROM "user" WHERE id = 4;
+
+-- todo  Deletion constraint on DELETE user
+-- Restrict Deletion -> ON DELETE RESTRICT / ON DELETE NO ACTION (default)
+-- Cascading Deletion -> ON DELETE CASCADE
+-- Setting NULL -> ON DELETE SET NULL
+-- Set Default value -> ON DELETE SET DEFAULT
